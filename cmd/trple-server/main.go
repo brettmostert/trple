5afe49ead65bec5eb5910c830f04fd3a9c54f322
Brett Mostert <brettmostert@gmail.com>
2021-11-27 20:20:00 +0200

    Broken commit - its getting interesting

--- cmd/trple-server/main.go
+++ cmd/trple-server/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"log"
+	"net"
 	"os"
+
+	productV1 "github.com/brettmostert/trple-proto-go/product/v1"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -13,4 +19,28 @@ func main() {
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
+	listenOn := "127.0.0.1:8080"
+	listener, err := net.Listen("tcp", listenOn)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s: %w", listenOn, err)
+	}
+
+	server := grpc.NewServer()
+	productV1.RegisterProductServiceServer(server, &productServiceServer{})
+	log.Println("Listening on", listenOn)
+	if err := server.Serve(listener); err != nil {
+		return fmt.Errorf("failed to serve gRPC server: %w", err)
+	}
+}
+
+type productServiceServer struct {
+	productV1.UnimplementedProductServiceServer
+}
+
+func (s *productServiceServer) PutPet(ctx context.Context, req *productV1.FetchResponseRequest) (*productV1.FetchResponseResponse, error) {
+	name := req.GetName()
+	petType := req.GetPetType()
+	log.Println("Got a request to create a", petType, "named", name)
+
+	return &productV1.FetchResponseResponse{}, nil
 }
